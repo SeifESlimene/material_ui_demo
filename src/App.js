237b2de8bf781928fbdbbc1f880c4fd903aad88d
seifeslimene <s.slimene19@gmail.com>
2023-02-05 16:33:39 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyApp from './MyApp'
+
+function App() {
+  return (
+    <div className="App">
+      <MyApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MyApp.js
@@ -0,0 +1,49 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+export default function MyApp() {
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+      <div style={{ margin: "20px", padding: "20px", backgroundColor: "#eee" }}>
+        <Accordion disableGutters={true} square={true}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>Accordion 1</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion disableGutters={true} square={true}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography>Accordion 2</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      </div>
+    </div>
+  );
+}
